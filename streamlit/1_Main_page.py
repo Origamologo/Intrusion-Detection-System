eeff32ac28cbc4471633c3bfb32ca4145370d70c
Origamologo <mituconservata@gmail.com>
2022-09-23 10:52:41 +0200

    streamlit for web

--- streamlit/1_Main_page.py
+++ streamlit/1_Main_page.py
@@ -20,7 +20,7 @@ st.markdown(" ")
 st.markdown(" ")
 
 # background = Image.open("https://github.com/Origamologo/Intrusion-Detection-System/blob/main/streamlit/imagenes/eye.webp")
-background = Image.open("imagenes/eye.webp")
+background = Image.open("eye.webp")
 col1, col2, col3 = st.columns([0.2, 5, 0.2])
 col2.image(background, use_column_width=True)
 
